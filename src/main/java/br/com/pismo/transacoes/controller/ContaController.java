8298f31e4f586a20123f77807c8d4984763b8773
João Felippe <jofelippe@hotmail.com>
2020-11-12 10:51:33 -0300

    Implementação do Swagger config.

--- src/main/java/br/com/pismo/transacoes/controller/ContaController.java
+++ src/main/java/br/com/pismo/transacoes/controller/ContaController.java
@@ -4,13 +4,16 @@ import br.com.pismo.transacoes.domain.Conta;
 import br.com.pismo.transacoes.dto.ContaDTO;
 import br.com.pismo.transacoes.mapper.ObjectMapper;
 import br.com.pismo.transacoes.service.ContaService;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/conta")
+@Api(value = "Conta")
 public class ContaController {
 
     private final ContaService contaService;
@@ -22,12 +25,24 @@ public class ContaController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Incluir conta")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Conta salva com sucesso", response = Object.class),
+            @ApiResponse(code = 400, message = "Conta já existente"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     public ResponseEntity incluirConta(@Valid @RequestBody ContaDTO conta) {
         contaService.incluirConta(mapper.convert(conta, Conta.class));
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{idConta}")
+    @ApiOperation(value = "Buscar conta", response = ContaDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Conta retornada"),
+            @ApiResponse(code = 404, message = "Alguma informação não foi encontrada"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     public ResponseEntity buscarConta(@PathVariable("idConta") Integer idConta) {
         ContaDTO conta = mapper.convert(contaService.buscarConta(idConta), ContaDTO.class);
         return ResponseEntity.ok(conta);
--- src/main/java/br/com/pismo/transacoes/controller/TransacaoController.java
+++ src/main/java/br/com/pismo/transacoes/controller/TransacaoController.java
@@ -4,6 +4,7 @@ package br.com.pismo.transacoes.controller;
 import br.com.pismo.transacoes.dto.TransacaoFinanceiraDTO;
 import br.com.pismo.transacoes.mapper.ObjectMapper;
 import br.com.pismo.transacoes.service.TransacaoService;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/transacao")
+@Api(value = "Transacao")
 public class TransacaoController {
 
     private final TransacaoService transacaoService;
@@ -25,6 +27,12 @@ public class TransacaoController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Criar transação")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Transação salva com sucesso", response = Object.class),
+            @ApiResponse(code = 404, message = "Alguma informação não foi encontrada"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     public ResponseEntity criarTransacao(@Valid @RequestBody TransacaoFinanceiraDTO transacaoFinanceiraDTO) {
         transacaoService.criarTransacao(transacaoFinanceiraDTO);
         return ResponseEntity.ok().build();
